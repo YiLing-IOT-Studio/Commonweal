b5c7d138acb38e396c4d036e2f88566cff52e9a7
zhyocean <1125694337@qq.com>
2018-05-03 15:35:54 +0800

    建立项目结构

---
+++ src/main/java/cn/zhyocean/CommonwealApplication.java
@@ -0,0 +1,12 @@
+package cn.zhyocean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonwealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonwealApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/zhyocean/CommonwealApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.zhyocean;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommonwealApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
